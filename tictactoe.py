1ebf750a6f6de1f14dab9697ec86e7cc260a9edc
Alyona Klekovkina <like.the.sunshine.17@gmail.com>
2020-09-16 17:16:16 +0300

    stage 4/5

--- tictactoe.py
+++ tictactoe.py
@@ -1,80 +1,45 @@
-# write your code here
-the_list = [n for n in input("Enter cells: ")]
-line_1 = the_list[0] + " " + the_list[1] + " " + the_list[2]
-line_2 = the_list[3] + " " + the_list[4] + " " + the_list[5]
-line_3 = the_list[6] + " " + the_list[7] + " " + the_list[8]
-print("""---------
-| {} |
-| {} |
-| {} |
----------""".format(line_1, line_2, line_3))
-
-list_of_x = []
-list_of_o = []
-for i in the_list:
-    if i == 'X':
-        list_of_x.append(i)
-    if i == 'O':
-        list_of_o.append(i)
-num_of_x = len(list_of_x)
-num_of_o = len(list_of_o)
-difference = abs(len(list_of_o) - len(list_of_x))
+def create_field():
+    the_list = [n for n in input("Enter cells: ")]
+    matrix = []
+    ap = 0
+    for i in range(3):
+        matrix.append([])
+        for j in range(3):
+            if the_list[ap] == "_":
+                matrix[i].append(" ")
+            else:
+                matrix[i].append(the_list[ap])
+            ap += 1
+    print("""---------\n| {} |\n| {} |\n| {} |\n---------""".format(" ".join(matrix[0]), " ".join(matrix[1]), " ".join(matrix[2])))
+    return matrix
 
 
-def check_combination_x():
-    if the_list[0] == 'X' and the_list[1] == 'X' and the_list[2] == 'X':
-        return "X wins"
-    if the_list[3] == 'X' and the_list[4] == 'X' and the_list[5] == 'X':
-        return "X wins"
-    if the_list[6] == 'X' and the_list[7] == 'X' and the_list[8] == 'X':
-        return "X wins"
-    if the_list[0] == 'X' and the_list[3] == 'X' and the_list[6] == 'X':
-        return "X wins"
-    if the_list[1] == 'X' and the_list[4] == 'X' and the_list[7] == 'X':
-        return "X wins"
-    if the_list[2] == 'X' and the_list[5] == 'X' and the_list[8] == 'X':
-        return "X wins"
-    if the_list[0] == 'X' and the_list[4] == 'X' and the_list[8] == 'X':
-        return "X wins"
-    if the_list[2] == 'X' and the_list[4] == 'X' and the_list[6] == 'X':
-        return "X wins"
-    else:
-        return False
+def get_coordinates():
+    while True:
+        row_column = input("Enter the coordinates: ")
+        try:
+            row, column = row_column.split()
+            if (int(row) > 3) or (int(column) > 3):
+                print("Coordinates should be from 1 to 3!")
+            elif the_field[3 - int(column)][int(row) - 1] == "X" or the_field[3 - int(column)][int(row) - 1] == "O":
+                print("This cell is occupied! Choose another one!")
+            elif (int(row) and int(column) == 1) or (int(row) and int(column) == 2) or (int(row) and int(column) == 3):
+                return int(row), int(column)
+        except ValueError:
+            print("You should enter numbers!")
+            continue
 
 
-def check_combination_o():
-    if the_list[0] == 'O' and the_list[1] == 'O' and the_list[2] == 'O':
-        return "O wins"
-    if the_list[3] == 'O' and the_list[4] == 'O' and the_list[5] == 'O':
-        return "O wins"
-    if the_list[6] == 'O' and the_list[7] == 'O' and the_list[8] == 'O':
-        return "O wins"
-    if the_list[0] == 'O' and the_list[3] == 'O' and the_list[6] == 'O':
-        return "O wins"
-    if the_list[1] == 'O' and the_list[4] == 'O' and the_list[7] == 'O':
-        return "O wins"
-    if the_list[2] == 'O' and the_list[5] == 'O' and the_list[8] == 'O':
-        return "O wins"
-    if the_list[0] == 'O' and the_list[4] == 'O' and the_list[8] == 'O':
-        return "O wins"
-    if the_list[2] == 'O' and the_list[4] == 'O' and the_list[6] == 'O':
-        return "O wins"
-    else:
-        return False
+def get_row_column():
+    column = coordinates[0] - 1
+    row = 3 - coordinates[1]
+    return row, column
 
 
-if difference == 0 or difference == 1:
-    check_combination_x()
-    check_combination_o()
-    if check_combination_o() and check_combination_x():
-        print("Impossible")
-    elif (num_of_o <= 3 and num_of_x <= 3) and (check_combination_o() is False and check_combination_x() is False):
-        print("Game not finished")
-    elif check_combination_o():
-        print(check_combination_o())
-    elif check_combination_x():
-        print(check_combination_x())
-    else:
-        print("Draw")
-if difference >= 2:
-    print("Impossible")
+the_field = create_field()
+coordinates = get_coordinates()
+r, c = get_row_column()
+cell = the_field[r][c]
+the_field[r][c] = "X"
+print("""---------\n| {} |\n| {} |\n| {} |\n---------""".format(" ".join(the_field[0]), " ".join(the_field[1]), " ".join(the_field[2])))
+
